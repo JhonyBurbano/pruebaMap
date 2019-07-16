5a271da8fc02cdd751622e1fbcc8b64a1ae67470
JhonyBurbano <jhonyburbanojb@gmail.com>
2019-07-16 16:18:14 -0500

    Add map with location

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPage implements OnInit {
 
+  // map: mapboxgl.Map;
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/outdoors-v9';
+  lat = 37.75;
+  lng = -122.41;
+
   constructor() { }
 
   ngOnInit() {
+    this.initializeMap();
   }
 
+  private initializeMap() {
+    /// locate the user
+    if (navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(position => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        this.map.flyTo({
+          center: [this.lng, this.lat]
+        });
+      });
+    }
+
+    this.buildMap();
+  }
+
+  buildMap() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiamhvbnlidXJiYW5vIiwiYSI6ImNqeTM5aXE3dDAyeTUzam45c2ZxcjI0eG4ifQ.kYXZtVTgpzMpitmIi2_Iag';
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: 13,
+      center: [this.lng, this.lat]
+    });
+
+    /// Add map controls
+    this.map.addControl(new mapboxgl.NavigationControl());
+
+  }
+
+// tslint:disable-next-line: eofline
 }
